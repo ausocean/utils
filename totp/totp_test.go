bdecf221e213ce4fa135c707e61b6ec98d558574
scruzin <anoble@gmail.com>
2024-07-17 10:52:50 +0930

    Added CheckTOTP.

--- totp/totp_test.go
+++ totp/totp_test.go
@@ -20,7 +20,7 @@ import (
 
 const secret = "not-so-secret"
 
-func TestTOTP(t *testing.T) {
+func TestGenerateTOTP(t *testing.T) {
 	nye2018 := time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC)
 
 	var tests = []struct {
@@ -58,6 +58,52 @@ func TestTOTP(t *testing.T) {
 		}
 		if totp != test.want {
 			t.Errorf("%d: expected %s TOTP, got %s", i, test.want, totp)
+		}
+	}
+}
+
+func TestCheckTOTP(t *testing.T) {
+	const digits = 16
+
+	now := time.Now()
+	var tests = []struct {
+		tm     time.Time
+		period time.Duration
+		want   bool
+	}{
+		{
+			tm:     now,
+			period: time.Duration(2 * time.Minute),
+			want:   true,
+		},
+		{
+			tm:     now,
+			period: time.Duration(time.Minute),
+			want:   true,
+		},
+		{
+			tm:     now,
+			period: time.Duration(0),
+			want:   true,
+		},
+		{
+			tm:     now.Add(2 * time.Minute),
+			period: time.Duration(0),
+			want:   false,
+		},
+	}
+
+	for i, test := range tests {
+		totp, err := GenerateTOTP(now, digits, []byte(secret))
+		if err != nil {
+			t.Fatalf("%d: GenerateTOTP returned unexpected error: %v", i, err)
+		}
+		ok, err := CheckTOTP(totp, test.tm, test.period, digits, []byte(secret))
+		if err != nil {
+			t.Fatalf("%d: CheckTOTP returned unexpected error: %v", i, err)
+		}
+		if ok != test.want {
+			t.Errorf("%d: expected %t got %t", i, test.want, ok)
 		}
 	}
 }
